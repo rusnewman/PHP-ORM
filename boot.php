0b614811fff6bedab11b3a468e94f98d2f4ed68a
Russell Newman <rn2@ecs.soton.ac.uk>
2011-04-21 22:20:41 +0100

    Added readme, added experimental class generator to bootstrapper.
    Signed-off-by: Russell Newman <rn2@ecs.soton.ac.uk>

--- boot.php
+++ boot.php
@@ -1,6 +1,14 @@
 <?
+	const AUTO_GENERATE_CLASSES = true;
+	
 	require_once("orm.class.php");
 	
 	function __autoload($class) {
-		if(file_exists("classes/$class.class.php")) require_once("classes/$class.class.php");
+		if(file_exists("classes/$class.class.php")) {
+			require_once("classes/$class.class.php");
+			return;
+		}
+		
+		// Experimental auto-class-generator as a last resort.
+		if(AUTO_GENERATE_CLASSES) @eval("class $class extends orm { }");
 	}
